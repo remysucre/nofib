9db22f4a1a16c5fb34aa6218280c1055ad132382
Yisu Wang <ywang30@cs.tufts.edu>
2016-03-15 01:11:29 -0400

    PARTIAL RESULT FOR GC

--- real/anna/BaseDefs.hs
+++ real/anna/BaseDefs.hs
@@ -1,260 +1,170 @@
-
--- ==========================================================--
--- === Base declarations                      BaseDefs.hs ===--
--- ==========================================================--
-
 module BaseDefs where
-
-----------------------------------------------------------
--- Useful polymorphic types                             --
-----------------------------------------------------------
-
+ 
 type AList a b = [(a, b)]
-
-type DefnGroup a = [(Bool, [a])] 
-
+ 
+type DefnGroup a = [(Bool, [a])]
+ 
 type ST a b = b -> (a, b)
-
+ 
 data ATree a b = ALeaf
                | ABranch (ATree a b) a b (ATree a b) Int
-                 deriving (Eq)
-     
---1.3:data Maybe a = Nothing 
---             | Just a 
---               deriving (Eq)
-
+               deriving Eq
+ 
 data Reply a b = Ok a
                | Fail b
-                 deriving (Eq)
-
-
-----------------------------------------------------------
--- Misc Utils stuff                                     --
-----------------------------------------------------------
-
+               deriving Eq
+ 
 type NameSupply = Int
-
+ 
 type Oseq = Int -> Int -> [Char]
-
+ 
 type Iseq = Oseq -> Oseq
-
+ 
 data Set a = MkSet [a]
-             deriving (Eq)
-  
+           deriving Eq
+ 
 type Bag a = [a]
-
-
-----------------------------------------------------------
--- Flags                                                --
-----------------------------------------------------------
-
-data Flag = Typecheck      -- don't do strictness analysis
-          | Simp           -- do HExpr-simplification (usually a bad idea)
-          | NoCaseOpt      -- don't do case-of-case optimisation
-          | ShowHExpr      -- print HExprs as they are generated
-          | NoPretty       -- don't clean up the program after \-lifting
-          | NoFormat       -- don't prettily format first order output
-          | NoBaraki       -- don't use embedding-closure pairs
-          | SimpleInv      -- use simplistic version of inverse
-          | PolyLim Int    -- how hard to work in Baraki dept for
-          | MonoLim Int    -- polymorphism and approx FPs respectively
-          | ForceAll       -- force all thunks before analysis
-          | DryRun         -- quick pass to check lattice table
-          | LowerLim Int   -- lower threshold for approx fixed pts
-          | UpperLim Int   -- upper threshold for approx fixed pts
-          | ScaleUp Int    -- scale up target ratio
-            deriving (Eq)
-
-bdDefaultSettings 
-   = [PolyLim 10000, MonoLim 10000, LowerLim 0, UpperLim 1000000, ScaleUp 20]
-
+ 
+data Flag = Typecheck
+          | Simp
+          | NoCaseOpt
+          | ShowHExpr
+          | NoPretty
+          | NoFormat
+          | NoBaraki
+          | SimpleInv
+          | PolyLim Int
+          | MonoLim Int
+          | ForceAll
+          | DryRun
+          | LowerLim Int
+          | UpperLim Int
+          | ScaleUp Int
+          deriving Eq
+bdDefaultSettings
+  = [PolyLim 10000, MonoLim 10000, LowerLim 0, UpperLim 1000000,
+     ScaleUp 20]
 bdDryRunSettings
-   = [NoBaraki, LowerLim 0, UpperLim 0, PolyLim 1, MonoLim 1, ScaleUp 20]
-
-
-----------------------------------------------------------
--- Provides a way for the system to give a              --
--- running commentary about what it is doing            --
-----------------------------------------------------------
-
-data SAInfo = SAResult    String Domain Route
-            | SASearch    ACMode String Int Int
-            | SASizes     String [OneFuncSize] [OneFuncSize]
-            | SAHExpr     String (HExpr Naam)
-            | SASL        [Route] [Route]
-            | SAGiveUp    [String]
-              deriving ()
-
-
-----------------------------------------------------------
--- Stuff for the Approx Fixed Pts business              --
-----------------------------------------------------------
-
+  = [NoBaraki, LowerLim 0, UpperLim 0, PolyLim 1, MonoLim 1,
+     ScaleUp 20]
+ 
+data SAInfo = SAResult String Domain Route
+            | SASearch ACMode String Int Int
+            | SASizes String [OneFuncSize] [OneFuncSize]
+            | SAHExpr String (HExpr Naam)
+            | SASL [Route] [Route]
+            | SAGiveUp [String]
+ 
 data ExceptionInt a = MkExInt Int [a]
-                      deriving (Eq, Ord, Show{-was:Text-})
-
-{- partain: moved from SmallerLattice.hs -}
-instance (Show{-was:Text-} a, Ord a) => Num (ExceptionInt a) where
-
-   (MkExInt i1 xs1) + (MkExInt i2 xs2) 
-      = MkExInt (i1 + i2) (xs1 ++ xs2)
-
-   (MkExInt i1 xs1) * (MkExInt i2 xs2) 
-      = MkExInt (i1 * i2) (xs1 ++ xs2)
-
+                    deriving (Eq, Ord, Show)
+ 
+instance (Show a, Ord a) => Num (ExceptionInt a) where
+        (MkExInt i1 xs1) + (MkExInt i2 xs2)
+          = MkExInt (i1 + i2) (xs1 ++ xs2)
+        (MkExInt i1 xs1) * (MkExInt i2 xs2)
+          = MkExInt (i1 * i2) (xs1 ++ xs2)
+ 
 type DomainInt = ExceptionInt Domain
-
+ 
 type DInt = (Domain, Int)
-
+ 
 type OneFuncSize = (Int, [Domain])
-
+ 
 type Sequence = ([[OneFuncSize]], [[OneFuncSize]])
-
-
-----------------------------------------------------------
--- Basic syntax trees for Core programs                 --
-----------------------------------------------------------
-
+ 
 type Naam = [Char]
-
+ 
 type Alter = AlterP Naam
-type AlterP a = ([a],                  -- parameters to pattern-match on
-                 CExprP a)             -- resulting expressions
-     
+ 
+type AlterP a = ([a], CExprP a)
+ 
 type ScValue = ScValueP Naam
-type ScValueP a = ([a],                -- list of arguments for the SC
-                   CExprP a)           -- body of the SC
-     
+ 
+type ScValueP a = ([a], CExprP a)
+ 
 type CoreProgram = CoreProgramP Naam
-type CoreProgramP a = ([TypeDef],      -- type definitions
-                       [(Naam,         -- list of SC names ...
-                         ScValueP a)]) --    and their definitions
-
-type AtomicProgram = ([TypeDef],       -- exactly like a CoreProgram except
-                      CExpr)           -- all the SCs are put into a letrec
-
-type TypeDef = (Naam,                  -- the type's name
-                [Naam],                -- schematic type variables
-                [ConstrAlt])           -- constructor list
-     
-type ConstrAlt = (Naam,                -- constructor's name
-                  [TDefExpr])          -- list of argument types
-
-data TDefExpr                          -- type expressions for definitions
-                = TDefVar Naam         -- type variables
-                | TDefCons             -- constructed types
-                     Naam              --    constructor's name
-                     [TDefExpr]        --    constituent type expressions
-                  deriving (Eq)
-
-
-----------------------------------------------------------
--- Core expressions                                     --
-----------------------------------------------------------
-
+ 
+type CoreProgramP a = ([TypeDef], [(Naam, ScValueP a)])
+ 
+type AtomicProgram = ([TypeDef], CExpr)
+ 
+type TypeDef = (Naam, [Naam], [ConstrAlt])
+ 
+type ConstrAlt = (Naam, [TDefExpr])
+ 
+data TDefExpr = TDefVar Naam
+              | TDefCons Naam [TDefExpr]
+              deriving Eq
+ 
 type CExpr = CExprP Naam
-
-data CExprP a                              -- Core expressions
-             = EVar Naam                   -- variables
-             | ENum Int                    -- numbers
-             | EConstr Naam                -- constructors
-             | EAp (CExprP a) (CExprP a)   -- applications
-             | ELet                        -- lets and letrecs
-                  Bool                     -- True == recursive
-                  [(a, CExprP a)] 
-                  (CExprP a)
-             | ECase                       -- case statements
-                  (CExprP a) 
-                  [(Naam, AlterP a)]
-             | ELam                        -- lambda abstractions
-                  [a]
-                  (CExprP a)
-               deriving (Eq)
-     
-     
-----------------------------------------------------------
--- Annotated Core expressions                           --
-----------------------------------------------------------
-     
+ 
+data CExprP a = EVar Naam
+              | ENum Int
+              | EConstr Naam
+              | EAp (CExprP a) (CExprP a)
+              | ELet Bool [(a, CExprP a)] (CExprP a)
+              | ECase (CExprP a) [(Naam, AlterP a)]
+              | ELam [a] (CExprP a)
+              deriving Eq
+ 
 type AnnExpr a b = (b, AnnExpr' a b)
-
-data AnnExpr' a b
-        = AVar Naam
-        | ANum Int
-        | AConstr Naam
-        | AAp (AnnExpr a b) (AnnExpr a b)
-        | ALet Bool [AnnDefn a b] (AnnExpr a b)
-        | ACase (AnnExpr a b) [AnnAlt a b]
-        | ALam [a] (AnnExpr a b)
-          deriving (Eq)
-
+ 
+data AnnExpr' a b = AVar Naam
+                  | ANum Int
+                  | AConstr Naam
+                  | AAp (AnnExpr a b) (AnnExpr a b)
+                  | ALet Bool [AnnDefn a b] (AnnExpr a b)
+                  | ACase (AnnExpr a b) [AnnAlt a b]
+                  | ALam [a] (AnnExpr a b)
+                  deriving Eq
+ 
 type AnnDefn a b = (a, AnnExpr a b)
-
-type AnnAlt a b  = (Naam, ([a], (AnnExpr a b)))
-
+ 
+type AnnAlt a b = (Naam, ([a], (AnnExpr a b)))
+ 
 type AnnProgram a b = [(Naam, [a], AnnExpr a b)]
-
-
-----------------------------------------------------------
--- Stuff for the #*$*%*%* Lambda-Lifter                 --
-----------------------------------------------------------
-
+ 
 data Eqn = EqnNVC Naam (Set Naam) (Set Naam)
-           deriving (Eq)
-
-
-----------------------------------------------------------
--- Typechecker stuff                                    --
-----------------------------------------------------------
-
-type TVName = ([Int],[Int])
-     
+         deriving Eq
+ 
+type TVName = ([Int], [Int])
+ 
 type Message = [Char]
-     
+ 
 data TExpr = TVar TVName
            | TArr TExpr TExpr
            | TCons [Char] [TExpr]
-             deriving (Eq)
-
+           deriving Eq
+ 
 data TypeScheme = Scheme [TVName] TExpr
-                  deriving (Eq)
-
+                deriving Eq
+ 
 type Subst = AList TVName TExpr
-     
+ 
 type TcTypeEnv = AList Naam TypeScheme
-     
+ 
 type TypeEnv = AList Naam TExpr
-     
+ 
 type TypeNameSupply = TVName
-     
+ 
 type TypeInfo = (Subst, TExpr, AnnExpr Naam TExpr)
-     
+ 
 type TypeDependancy = DefnGroup Naam
-     
-     
-----------------------------------------------------------
--- Domain stuff                                         --
-----------------------------------------------------------
--- Assumes that all Domain values passed are in         --
--- uncurried form, ie no (Func _ (Func _ _)).           --
--- Functions generating denormalised                    --
--- function Domains must normalise them themselves.     --
-----------------------------------------------------------
-
+ 
 type Point = (Domain, Route)
-
+ 
 data FrontierElem = MkFrel [Route]
-                    deriving (Eq, Ord, Show{-was:Text-})
-
+                  deriving (Eq, Ord, Show)
+ 
 data Frontier = Min1Max0 Int [FrontierElem] [FrontierElem]
-                deriving (Eq, Ord, Show{-was:Text-})
-
+              deriving (Eq, Ord, Show)
+ 
 data Domain = Two
             | Lift1 [Domain]
             | Lift2 [Domain]
-            | Func  [Domain] Domain
-              deriving (Eq, Ord, Show, Read)
-
+            | Func [Domain] Domain
+            deriving (Eq, Ord, Show, Read)
+ 
 data Route = Zero
            | One
            | Stop1
@@ -263,144 +173,71 @@ data Route = Zero
            | Up2
            | UpUp2 [Route]
            | Rep Rep
-             deriving (Eq, Ord, Show{-was:Text-})
-
+           deriving (Eq, Ord, Show)
+ 
 data Rep = RepTwo Frontier
          | Rep1 Frontier [Rep]
          | Rep2 Frontier Frontier [Rep]
-           deriving (Eq, Ord, Show{-was:Text-})
-
-data DExpr = DXTwo                        
-           | DXLift1  [DExpr]         
-           | DXLift2  [DExpr]
-           | DXFunc   [DExpr] DExpr 
-           | DXVar    String
-             deriving (Eq)
-
+         deriving (Eq, Ord, Show)
+ 
+data DExpr = DXTwo
+           | DXLift1 [DExpr]
+           | DXLift2 [DExpr]
+           | DXFunc [DExpr] DExpr
+           | DXVar String
+           deriving Eq
+ 
 type RSubst = AList String Route
-
+ 
 type DSubst = AList String Domain
-
+ 
 type DRRSubst = AList String (Domain, Route, Route)
-
+ 
 type DExprEnv = AList String DExpr
-     
+ 
 data ConstrElem = ConstrRec
                 | ConstrVar Int
-                  deriving (Eq, Ord, Show{-was:Text-})
-
-
-----------------------------------------------------------
--- Abs and Conc stuff                                   --
-----------------------------------------------------------
-
+                deriving (Eq, Ord, Show)
+ 
 data ACMode = Safe
             | Live
-              deriving (Eq)
-
-----------------------------------------------------------
--- Frontier search stuff                                --
-----------------------------------------------------------
-
+            deriving Eq
+ 
 type MemoList = AList [Route] Route
-
-data AppInfo = A2 
-                   -- trivial case
-             | ALo1 
-                   -- low factor in function to Lift1
+ 
+data AppInfo = A2
+             | ALo1
              | AHi1 Int Int Domain
-                   -- a high factor in a function to Lift1.
-                   -- First Int is arity of low factor, second is
-                   -- the index of the high factor sought.
-                   -- Domain is of the high factor sought.
              | ALo2
-                   -- low factor in function to Lift2
              | AMid2
-                   -- middle factor in function to Lift2
              | AHi2 Int Int Domain
-                   -- a high factor in a function to Lift1.
-                   -- First Int is arity of low & middle factors,
-                   -- second is the index of the high factor sought.
-                   -- Domain is of high factor sought.
-               deriving (Eq)
-
-
-----------------------------------------------------------
--- Abstract expression trees                            --
-----------------------------------------------------------
-
+             deriving Eq
+ 
 data HExpr a = HApp (HExpr a) (HExpr a)
              | HVAp (HExpr a) [HExpr a]
              | HLam [a] (HExpr a)
              | HVar a
-             | HMeet [HExpr a]  -- must be at least one in list
+             | HMeet [HExpr a]
              | HPoint Route
              | HTable (AList Route (HExpr a))
-               deriving (Eq, Show{-was:Text-})
-
-     
-----------------------------------------------------------
--- Prettyprinter stuff                                  --
-----------------------------------------------------------
-     
-type PrPoint =  [Int]
-     
-type PrDomain =  [PrPoint]
-     
-     
-----------------------------------------------------------
--- Parser stuff                                         --
-----------------------------------------------------------
-     
-type Token =  (Int, [Char])
-
+             deriving (Eq, Show)
+ 
+type PrPoint = [Int]
+ 
+type PrDomain = [PrPoint]
+ 
+type Token = (Int, [Char])
+ 
 data PResult a = PFail [Token]
                | POk a [Token]
-                 deriving (Eq)
-
-type Parser a =  [Token] -> PResult a
-     
-data PartialExpr = NoOp 
+               deriving Eq
+ 
+type Parser a = [Token] -> PResult a
+ 
+data PartialExpr = NoOp
                  | FoundOp Naam CExpr
-                   deriving (Eq)
-
-
--- ===============================================================--
--- === Definition of the static component                      ===--
--- ===---------------------------------------------------------===--
--- === The static component carries around all information     ===--
--- === which remains unchanged throughout strictness analysis. ===--
--- === This avoids having to pass around vast hordes of        ===--
--- === parameters containing static information.               ===--
--- ===============================================================--
-     
-type StaticComponent 
-    =  ( 
-     	 DExprEnv,  
-     	 -- == AList Naam DExpr, the program's types
-
-         DSubst,
-         -- == AList Naam Domain, the simplest domains of functions
-
-         AList Naam [ConstrElem],
-         -- information on constructors
-   
-         AList Naam [Naam],
-         -- information on pseudo-params inserted to fix free vars
-
-         [Flag],
-         -- set of flags altering system operation
-
-         (Int, Int, Int, Int, Int),
-         -- polymorphic and monomorphic Baraki limits, 
-         -- and lower and upper limits for lattice sizes
-         -- and the scaleup ratio
-
-         AList Domain Int
-         -- the lattice size table
-        )
-
-
--- ==========================================================--
--- === end                                    BaseDefs.hs ===--
--- ==========================================================--
+                 deriving Eq
+ 
+type StaticComponent =
+     (DExprEnv, DSubst, AList Naam [ConstrElem], AList Naam [Naam],
+      [Flag], (Int, Int, Int, Int, Int), AList Domain Int)
